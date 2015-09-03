309715c1c409ba377f8fddaf606c96d354fde353
Donatello Ferraz <donatelloferraz@hotmail.com>
2015-09-03 13:53:05 -0300

    Atualização

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Donatello
+ *
+ * Created on 27 de Agosto de 2015, 14:01
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../arq/mongoose.h"
+#include "../arq/sqlite3.h"
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
