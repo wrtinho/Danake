f4f94a9eb9a2705f0a257120e821908b5d9fa5b3
George Candido <george.faria10@gmail.com>
2015-10-20 20:34:17 -0200

    Criação de conexão e manipulação dos dados no banco de dados
    Arquivo criado para manipular informações com o banco de dados do
    SQLite3.
    
    Retiramos como base, o exemplo do tutorial:
    http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm

---
+++ database/CMakeFiles/db.dir/DependInfo.cmake
@@ -0,0 +1,17 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/Repositorios/Danake/database/db.c" "/cygdrive/d/Repositorios/Danake/database/CMakeFiles/db.dir/db.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
